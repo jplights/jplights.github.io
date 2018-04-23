7e156741fb353d245056ff4e7d9457f32f818538
jplights <38489795+jplights@users.noreply.github.com>
2018-04-23 16:56:56 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -59,5 +59,18 @@
     distance: '0px'
   }, 300);
 
+    var offsetTop = $('#skills').offset().top;
+  $(window).scroll(function() {
+  var height = $(window).height();
+  if($(window).scrollTop()+height > offsetTop) {
+    jQuery('.skillbar').each(function(){
+      jQuery(this).find('.skillbar-bar').animate({
+        width:jQuery(this).attr('data-percent')
+      },2000);
+    });
+  }
+  });
+
+
 })(jQuery); // End of use strict
 
